cd33fb52d148c7a6cd9120203246100b21e3a849
Amine Bouaggad <Abouaggad@omnidata.ma>
2022-07-19 15:38:17 +0100

    add Integration Test

---
+++ client-service/src/test/java/ma/omnishore/clients/integration/ClientIntegrationTests.java
@@ -0,0 +1,36 @@
+package ma.omnishore.clients.integration;
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import ma.omnishore.clients.api.ClientController;
+import ma.omnishore.clients.domain.Client;
+ 
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@SpringBootTest
+class ClientIntegrationTests {
+ 
+  @Autowired
+  ClientController clientController;
+ 
+  @Test
+  void testCreateReadDelete() {
+	  Client client = new Client("BOUAGGAD", "Amine", "abouaggad@omnidata.ma", "Casablanca");
+ 
+    ResponseEntity<Client> clientResult = clientController.createClient(client);
+ 
+    ResponseEntity<List<Client>> clients = clientController.findAll();
+	Assertions.assertThat(clients.getBody()).isNotEmpty();
+ 
+    clientController.deleteClient(clientResult.getBody().getId());
+    Assertions.assertThat(clientController.findAll().getBody()).isEmpty();
+  }
+}
